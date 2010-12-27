d8a5c2b7d0167d64ed74c8db1dfbf315806ea009
Ariya Hidayat <ariya.hidayat@gmail.com>
2010-12-26 23:46:50 -0800

    tweets.js: Inevitable example of showing twitter status.

---
+++ examples/tweets.js
@@ -0,0 +1,12 @@
+// Get twitter status from someone
+
+if (phantom.storage.length === 0) {
+    phantom.storage = 'tweets';
+    phantom.open('http://mobile.twitter.com/mr3bc');
+} else {
+    var list = document.querySelectorAll('span.status');
+    for (var i = 0; i < list.length; ++i) {
+        phantom.log((i + 1) + ': ' + list[i].innerHTML.replace(/<.*?>/g, ''));
+    }
+    phantom.exit();
+}
