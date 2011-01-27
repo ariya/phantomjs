4de99130c52b9ca6bff39b8f6e345ea4e949acc2
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-01-27 08:52:21 -0800

    Add driver for running QUnit test suite.
    Courtesy of Łukasz Korecki (https://gist.github.com/796548).

---
+++ examples/run-qunit.js
@@ -0,0 +1,21 @@
+if (phantom.state.length === 0) {
+    if (phantom.args.length === 0 || phantom.args.length > 2) {
+        console.log('Usage: run-qunit.js URL');
+        phantom.exit();
+    } else {
+        phantom.state = 'run-qunit';
+        phantom.open(phantom.args[0]);
+    }
+} else {
+    setInterval(function() {
+        var el = document.getElementById('qunit-testresult');
+        if (el && el.innerText.match('completed')) {
+            console.log(el.innerText);
+            try {
+                failed = el.getElementsByClassName('failed')[0].innerHTML;
+            } catch (e) {
+            }
+            phantom.exit((parseInt(failed, 10) > 0) ? 1 : 0);
+        }
+    }, 100);
+}
