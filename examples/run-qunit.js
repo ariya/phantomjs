32c5f02f2bcfeb4d4e54bc749cff24df95bc3162
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-01-28 08:24:11 -0800

    QUnit driver should not print the status multiple times.
    If the test suite reloads the page over and over again, only one
    final message should be displayed. Detect this by adding one more
    state to the logic.

--- examples/run-qunit.js
+++ examples/run-qunit.js
@@ -9,13 +9,16 @@ if (phantom.state.length === 0) {
 } else {
     setInterval(function() {
         var el = document.getElementById('qunit-testresult');
-        if (el && el.innerText.match('completed')) {
-            console.log(el.innerText);
-            try {
-                failed = el.getElementsByClassName('failed')[0].innerHTML;
-            } catch (e) {
+        if (phantom.state !== 'finish') {
+            if (el && el.innerText.match('completed')) {
+                phantom.state = 'finish';
+                console.log(el.innerText);
+                try {
+                    failed = el.getElementsByClassName('failed')[0].innerHTML;
+                } catch (e) {
+                }
+                phantom.exit((parseInt(failed, 10) > 0) ? 1 : 0);
             }
-            phantom.exit((parseInt(failed, 10) > 0) ? 1 : 0);
         }
     }, 100);
 }
