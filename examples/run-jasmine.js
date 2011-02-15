2e628b1bca7d8e603edc78b4273fed495bb5fc04
Greg Musick <greg.musick@gmail.com>
2011-02-14 23:05:59 -0800

    Load spec runner from file system. Set up optional file writer.
    - Spec runner html file can be loaded up as a local file rather than a url
    - window.fileWriter.write() API is set up so a jasmine reporter could
      write out its results to a file for CI
    - Get run results from Jasmine API rather than html scraping

--- examples/run-jasmine.js
+++ examples/run-jasmine.js
@@ -1,26 +1,55 @@
-if (phantom.state.length === 0) {
-    if (phantom.args.length !== 1) {
-        console.log('Usage: run-jasmine.js URL');
-        phantom.exit();
-    } else {
-        phantom.state = 'run-jasmine';
-        phantom.open(phantom.args[0]);
-    }
-} else {
-    window.setInterval(function () {
-        var list, el, desc, i, j;
-        if (document.body.querySelector('.finished_at')) {
-            console.log(document.body.querySelector('.description').innerText);
-            list = document.body.querySelectorAll('div.jasmine_reporter > div.suite.failed');
-            for (i = 0; i < list.length; ++i) {
-                el = list[i];
-                desc = el.querySelectorAll('.description');
-                console.log('');
-                for (j = 0; j < desc.length; ++j) {
-                    console.log(desc[j].innerText);
+var logSpecFailures = function(specs) {
+    for (var i = 0; i < specs.length; i++) {
+        var spec = specs[i];
+
+        var results = spec.results();
+        if (!results.passed()) {
+            console.log("");
+            console.log(spec.suite.getFullName() + " " + spec.description + " failed:");
+
+            var items = results.getItems();
+            for (var j = 0; j < items.length; j++) {
+                var result = items[j];
+                if (result.passed && !result.passed()) {
+                  console.log(result.toString());
                 }
             }
-            phantom.exit();
+        }
+    }
+};
+
+if (phantom.state.length === 0) {
+    if (phantom.args.length < 1) {
+        console.log("Usage: phantomjs.exe run-jasmine.js path/to/spec/runner/html/file [output file]");
+        phantom.exit(1);
+    } else {
+        phantom.state = "run-jasmine";
+        phantom.openFile(phantom.args[0]);
+    }
+} else {
+    var file = phantom.args[1];
+    window.fileWriter = {
+        write: function(content) {
+            if (file) {
+                phantom.writeToFile(file, content);
+            }
+        }
+    };
+
+    window.setInterval(function () {
+        var status = document.body.querySelector(".runner .description");
+        if (status && window.jasmine) {
+            var runner = window.jasmine.getEnv().currentRunner();
+
+            var failures = runner.results().failedCount;
+            if (failures) {
+                logSpecFailures(runner.specs());
+            }
+
+            console.log("");
+            console.log(status.innerText);
+
+            phantom.exit(failures ? 1 : 0);
         }
     }, 100);
 }
