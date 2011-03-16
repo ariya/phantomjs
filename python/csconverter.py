a731e6c4d72cf0507d3daf272ee852491e9cdb27
IceArmy <roejames12@hotmail.com>
2011-03-15 22:18:01 -0700

    Added Python PhantomJS project

---
+++ python/csconverter.py
@@ -0,0 +1,43 @@
+'''
+  This file is part of the PyPhantomJS project.
+
+  Copyright (C) 2011 James Roe <roejames12@hotmail.com>
+  Copyright (C) 2010-2011 Ariya Hidayat <ariya.hidayat@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import sys, resources
+from PyQt4.QtCore import QObject, QFile, QVariant, QString
+from PyQt4.QtWebKit import QWebPage, QWebFrame
+
+class CSConverter(QObject):
+    def __init__(self, parent = None):
+        QObject.__init__(self, parent)
+        self.m_webPage = QWebPage(self)
+
+        converter = QFile(':/resources/coffee-script.js')
+        converter.open(QFile.ReadOnly)
+
+        script = QString.fromUtf8(converter.readAll())
+        converter.close()
+        self.m_webPage.mainFrame().evaluateJavaScript(script)
+        self.m_webPage.mainFrame().addToJavaScriptWindowObject('converter', self)
+
+    def convert(self, script):
+        self.setProperty('source', script)
+        result = self.m_webPage.mainFrame().evaluateJavaScript('this.CoffeeScript.compile(converter.source)')
+        if result.type() == QVariant.String:
+            return result.toString()
+        return QString()
