1bdd1867ff0523bf3b085165b1014ef24fb27dee
IceArmy <roejames12@hotmail.com>
2011-04-12 01:37:47 -0700

    Remove some extra spacing on the header

--- python/webpage.py
+++ python/webpage.py
@@ -16,7 +16,7 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-''' 
+'''
 
 from PyQt4.QtCore import *
 from PyQt4.QtWebKit import *
