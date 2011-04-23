0a91497075178c069e6959759db98bc648d5bb0f
rgieseke <robert.gieseke@gmail.com>
2011-04-23 14:41:19 +0200

    Loop only through divs, not length and item.

--- examples/pizza.js
+++ examples/pizza.js
@@ -5,7 +5,7 @@ if (phantom.state.length === 0) {
     phantom.open('http://www.google.com/m/local?site=local&q=pizza+in+new+york');
 } else {
     var list = document.querySelectorAll('div.bf');
-    for (var i in list) {
+    for (var i = 0; i < list.length; i++) {
         console.log(list[i].innerText);
     }
     phantom.exit();
