5f018b32b2e6e421071a2725f2d52c4fe0e7ed12
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-05-25 14:29:20 -0700

    useragent.js: Port to WebPage object.

--- examples/useragent.js
+++ examples/useragent.js
@@ -1,8 +1,13 @@
-if (phantom.state.length === 0) {
-    phantom.state = 'checking';
-    phantom.userAgent = 'SpecialAgent';
-    phantom.open('http://www.httpuseragent.org');
-} else {
-    console.log(document.getElementById('myagent').innerText);
+var page = new WebPage();
+
+page.open('http://www.httpuseragent.org', function (status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
+    } else {
+        var ua = page.evaluate(function () {
+            return document.getElementById('myagent').innerText;
+        });
+        console.log(ua);
+    }
     phantom.exit();
-}
+});
