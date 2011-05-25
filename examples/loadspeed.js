a1f0691311c3e821ca6f91162b7448f0ef239f1e
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-05-25 14:00:22 -0700

    loadspeed.js: Port to use WebPage object.

--- examples/loadspeed.js
+++ examples/loadspeed.js
@@ -1,20 +1,22 @@
-if (phantom.state.length === 0) {
-    if (phantom.args.length === 0) {
-        console.log('Usage: loadspeed.js <some URL>');
-        phantom.exit();
-    } else {
-        var address = phantom.args[0];
-        phantom.state = Date.now().toString();
-        console.log('Loading ' + address);
-        phantom.open(address);
-    }
-} else {
-    var elapsed = Date.now() - new Date().setTime(phantom.state);
-    if (phantom.loadStatus === 'success') {
-        console.log('Page title is ' + document.title);
-        console.log('Loading time ' + elapsed + ' msec');
-    } else {
-        console.log('FAIL to load the address');
-    }
+var page = new WebPage(),
+    t, address;
+
+if (phantom.args.length === 0) {
+    console.log('Usage: loadspeed.js <some URL>');
     phantom.exit();
+} else {
+    t = Date.now(),
+    address = phantom.args[0],
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address');
+        } else {
+            t = Date.now() - t;
+            console.log('Page title is ' + page.evaluate(function () {
+                return document.title;
+            }));
+            console.log('Loading time ' + t + ' msec');
+        }
+        phantom.exit();
+    });
 }
