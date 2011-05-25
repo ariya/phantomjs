5da05055cc0beae4fa774298416ba44d879a55e5
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-05-25 14:16:41 -0700

    pizza.js: Port to use WebPage object.

--- examples/pizza.js
+++ examples/pizza.js
@@ -1,12 +1,20 @@
 // Find pizza in New York using Google Local
 
-if (phantom.state.length === 0) {
-    phantom.state = 'pizza';
-    phantom.open('http://www.google.com/m/local?site=local&q=pizza+in+new+york');
-} else {
-    var list = document.querySelectorAll('div.bf');
-    for (var i = 0; i < list.length; i++) {
-        console.log(list[i].innerText);
+var page = new WebPage(),
+    results = [];
+
+page.open('http://www.google.com/m/local?site=local&q=pizza+in+new+york', function (status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
+    } else {
+        results = page.evaluate(function() {
+            var list = document.querySelectorAll('div.bf'), pizza = [], i;
+            for (i = 0; i < list.length; i++) {
+                pizza.push(list[i].innerText);
+            }
+            return pizza;
+        });
+        console.log(results.join('\n'));
     }
     phantom.exit();
-}
+});
