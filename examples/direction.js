838b110e9ea2e109f690b7c8aa82929a174e27d2
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-05-25 14:10:35 -0700

    direction.js: Port to use WebPage object.

--- examples/direction.js
+++ examples/direction.js
@@ -1,34 +1,34 @@
 // Get driving direction using Google Directions API.
 
-if (phantom.state.length === 0) {
-    var origin, dest;
-    if (phantom.args.length < 2) {
-        console.log('Usage: direction.js origin destination');
-        console.log('Example: direction.js "San Diego" "Palo Alto"');
-        phantom.exit(1);
-    }
+var page = new WebPage(),
+    origin, dest, steps;
+
+if (phantom.args.length < 2) {
+    console.log('Usage: direction.js origin destination');
+    console.log('Example: direction.js "San Diego" "Palo Alto"');
+    phantom.exit(1);
+} else {
     origin = phantom.args[0];
     dest = phantom.args[1];
-    phantom.state = origin + ' to ' + dest;
-    phantom.open(encodeURI('http://maps.googleapis.com/maps/api/directions/xml?origin=' + origin +
-                '&destination=' + dest + '&units=imperial&mode=driving&sensor=false'));
-} else {
-    if (phantom.loadStatus === 'fail') {
-        console.log('Unable to access network');
-    } else {
-        var steps = phantom.content.match(/<html_instructions>(.*)<\/html_instructions>/ig);
-        if (steps == null) {
-            console.log('No data available for ' + phantom.state);
+    page.open(encodeURI('http://maps.googleapis.com/maps/api/directions/xml?origin=' + origin +
+                '&destination=' + dest + '&units=imperial&mode=driving&sensor=false'), function (status) {
+        if (status !== 'success') {
+            console.log('Unable to access network');
         } else {
-            steps.forEach(function (ins) {
-                ins = ins.replace(/\&lt;/ig, '<').replace(/\&gt;/ig, '>');
-                ins = ins.replace(/\<div/ig, '\n<div');
-                ins = ins.replace(/<.*?>/g, '');
-                console.log(ins);
-            });
-            console.log('');
-            console.log(phantom.content.match(/<copyrights>.*<\/copyrights>/ig).join('').replace(/<.*?>/g, ''));
+            steps = page.content.match(/<html_instructions>(.*)<\/html_instructions>/ig);
+            if (steps == null) {
+                console.log('No data available for ' + phantom.state);
+            } else {
+                steps.forEach(function (ins) {
+                    ins = ins.replace(/\&lt;/ig, '<').replace(/\&gt;/ig, '>');
+                    ins = ins.replace(/\<div/ig, '\n<div');
+                    ins = ins.replace(/<.*?>/g, '');
+                    console.log(ins);
+                });
+                console.log('');
+                console.log(page.content.match(/<copyrights>.*<\/copyrights>/ig).join('').replace(/<.*?>/g, ''));
+            }
         }
-    }
-    phantom.exit();
+        phantom.exit();
+    });
 }
