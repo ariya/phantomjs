f84bef4b38ed51e9bdca8aa784d7b945d2d028eb
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-05-25 14:23:10 -0700

    rasterize.js: Port to WebPage object.

--- examples/pizza.js
+++ examples/pizza.js
@@ -1,13 +1,12 @@
 // Find pizza in New York using Google Local
 
-var page = new WebPage(),
-    results = [];
+var page = new WebPage();
 
 page.open('http://www.google.com/m/local?site=local&q=pizza+in+new+york', function (status) {
     if (status !== 'success') {
         console.log('Unable to access network');
     } else {
-        results = page.evaluate(function() {
+        var results = page.evaluate(function() {
             var list = document.querySelectorAll('div.bf'), pizza = [], i;
             for (i = 0; i < list.length; i++) {
                 pizza.push(list[i].innerText);
--- examples/rasterize.js
+++ examples/rasterize.js
@@ -1,22 +1,27 @@
-if (phantom.state.length === 0) {
-    if (phantom.args.length < 2 || phantom.args.length > 3) {
-        console.log('Usage: rasterize.js URL filename [paperwidth*paperheight|paperformat]');
-        console.log('  paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"');
-        phantom.exit();
-    } else {
-        var address = phantom.args[0];
-        phantom.state = 'rasterize';
-        phantom.viewportSize = { width: 600, height: 600 };
-        if (phantom.args.length === 3 && phantom.args[1].substr(-4) === ".pdf") {
-            var size = phantom.args[2].split('*');
-            phantom.paperSize = size.length === 2 ? { width: size[0], height: size[1], border: '0px' }
-                                                  : { format: phantom.args[2], orientation: 'portrait', border: '1cm' };
-        }
-        phantom.open(address);
-    }
-} else {
-    var output = phantom.args[1];
-    phantom.sleep(200);
-    phantom.render(output);
+var page = new WebPage(),
+    address, output, size;
+
+if (phantom.args.length < 2 || phantom.args.length > 3) {
+    console.log('Usage: rasterize.js URL filename [paperwidth*paperheight|paperformat]');
+    console.log('  paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"');
     phantom.exit();
+} else {
+    address = phantom.args[0];
+    output = phantom.args[1];
+    page.viewportSize = { width: 600, height: 600 };
+    if (phantom.args.length === 3 && phantom.args[1].substr(-4) === ".pdf") {
+        size = phantom.args[2].split('*');
+        phantom.paperSize = size.length === 2 ? { width: size[0], height: size[1], border: '0px' }
+                                              : { format: phantom.args[2], orientation: 'portrait', border: '1cm' };
+    }
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('Unable to load the address!');
+        } else {
+            window.setTimeout(function () {
+                page.render(output);
+                phantom.exit();
+            }, 200);
+        }
+    });
 }
