d2c520a77773303fcfcabda613a11d4f49ffaf1c
IceArmy <roejames12@hotmail.com>
2011-06-01 02:09:07 -0700

    Fix bug where WebPage.evaluate had a syntax error.
    It now creates a proper anonymous function (which passes this for global context)

--- python/webpage.py
+++ python/webpage.py
@@ -269,7 +269,7 @@ class WebPage(QObject):
 
     @pyqtSlot(str, result='QVariant')
     def evaluate(self, code):
-        function = '(%s)()' % code
+        function = '(function(){%s}).call(this);' % code
         return self.m_mainFrame.evaluateJavaScript(function)
 
     @pyqtSlot(str, str, 'QVariantMap')
