7069b707abe3f5671c5f2fde61af43fd28c6ef83
Francisco Souza <francisco@franciscosouza.net>
2011-06-03 16:21:03 -0300

    Added setup.py and phantom package (for PyPI)

---
+++ python/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from phantom import __version__
+
+README = open('README').read()
+
+setup(name='phantom.py',
+      version=__version__,
+      description='Headless WebKit with Python API',
+      long_description=README,
+      author='Author name',
+      author_email='author-email',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=['PyQt4'],
+      )
