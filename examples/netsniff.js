c2c6cbc287e52c7214c4c24a6dbbd36e98c40fa9
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-06-08 23:45:09 -0700

    example/netsniff.js: Format the output to HAR 1.2 specification.

--- examples/netsniff.js
+++ examples/netsniff.js
@@ -1,32 +1,130 @@
-var page = new WebPage(), address, requests = [], responses = [];
+if (!Date.prototype.toISOString) {
+    Date.prototype.toISOString = function () {
+        function pad(n) { return n < 10 ? '0' + n : n; }
+        function ms(n) { return n < 10 ? '00'+ n : n < 100 ? '0' + n : n }
+        return this.getFullYear() + '-' +
+            pad(this.getMonth() + 1) + '-' +
+            pad(this.getDate()) + 'T' +
+            pad(this.getHours()) + ':' +
+            pad(this.getMinutes()) + ':' +
+            pad(this.getSeconds()) + '.' +
+            ms(this.getMilliseconds()) + 'Z';
+    }
+}
+
+function createHAR(address, title, startTime, resources)
+{
+    var entries = [];
+
+    resources.forEach(function (resource) {
+        var request = resource.request,
+            startReply = resource.startReply,
+            endReply = resource.endReply;
+
+        if (!request || !startReply || !endReply) {
+            return;
+        }
+
+        entries.push({
+            startedDateTime: request.time.toISOString(),
+            time: endReply.time - request.time,
+            request: {
+                method: request.method,
+                url: request.url,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: request.headers,
+                queryString: [],
+                headersSize: -1,
+                bodySize: -1
+            },
+            response: {
+                status: endReply.status,
+                statusText: endReply.statusText,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: endReply.headers,
+                redirectURL: "",
+                headersSize: -1,
+                bodySize: startReply.bodySize,
+                content: {
+                    size: startReply.bodySize,
+                    mimeType: endReply.contentType
+                }
+            },
+            cache: {},
+            timings: {
+                blocked: 0,
+                dns: -1,
+                connect: -1,
+                send: 0,
+                wait: startReply.time - request.time,
+                receive: endReply.time - startReply.time,
+                ssl: -1
+            }
+        });
+    });
+
+    return {
+        log: {
+            version: '1.2',
+            creator: {
+                name: "PhantomJS",
+                version: phantom.version.major + '.' + phantom.version.minor +
+                    '.' + phantom.version.patch
+            },
+            pages: [{
+                startedDateTime: page.startTime.toISOString(),
+                id: address,
+                title: title,
+                pageTimings: {}
+            }],
+            entries: entries
+        }
+    };
+}
+
+var page = new WebPage();
 
 if (phantom.args.length === 0) {
     console.log('Usage: netsniff.js <some URL>');
     phantom.exit();
 } else {
-    address = phantom.args[0];
+
+    page.address = phantom.args[0];
+    page.resources = [];
 
     page.onLoadStarted = function () {
-        page.startTime = Date.now();
+        page.startTime = new Date();
     };
 
     page.onResourceRequested = function (req) {
-        requests.push(req);
+        page.resources[req.id] = {
+            request: req,
+            startReply: null,
+            endReply: null
+        };
     };
 
     page.onResourceReceived = function (res) {
-        responses.push(res);
+        if (res.stage === 'start') {
+            page.resources[res.id].startReply = res;
+        }
+        if (res.stage === 'end') {
+            page.resources[res.id].endReply = res;
+        }
     };
 
-    page.open(address, function (status) {
+    page.open(page.address, function (status) {
+        var har;
         if (status !== 'success') {
             console.log('FAIL to load the address');
         } else {
-            console.log('All requests:');
-            console.log(JSON.stringify(requests, undefined, 4));
-            console.log();
-            console.log('All responses:');
-            console.log(JSON.stringify(responses, undefined, 4));
+            page.title = page.evaluate(function () {
+                return document.title;
+            });
+            har = createHAR(page.address, page.title, page.startTime, page.resources);
+            console.log(JSON.stringify(har, undefined, 4));
         }
         phantom.exit();
     });
