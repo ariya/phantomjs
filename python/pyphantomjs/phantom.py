a2aed0064c852ba9bc57de421b1294565da6e5d2
IceArmy <roejames12@hotmail.com>
2011-06-28 18:39:29 -0700

    Fix crash on exit (Issues #136, #148 and #149)

--- python/pyphantomjs/phantom.py
+++ python/pyphantomjs/phantom.py
@@ -39,6 +39,7 @@ class Phantom(QObject):
 
         # variable declarations
         self.m_defaultPageSettings = {}
+        self.m_pages = []
         self.m_verbose = args.verbose
         self.m_page = WebPage(self)
         self.m_returnValue = 0
@@ -46,6 +47,8 @@ class Phantom(QObject):
         # setup the values from args
         self.m_scriptFile = args.script
         self.m_args = args.script_args
+
+        self.m_pages.append(self.m_page)
 
         do_action('PhantomInitPre', Bunch(locals()))
 
@@ -106,6 +109,7 @@ class Phantom(QObject):
     @pyqtSlot(result=WebPage)
     def createWebPage(self):
         page = WebPage(self)
+        self.m_pages.append(page)
         page.applySettings(self.m_defaultPageSettings)
         page.setNetworkAccessManager(self.m_netAccessMan)
         page.libraryPath = os.path.dirname(os.path.abspath(self.m_scriptFile))
@@ -121,10 +125,12 @@ class Phantom(QObject):
         self.m_terminated = True
         self.m_returnValue = code
 
-        # stop javascript execution; delete C++ object first,
-        # then delete the Python reference
-        sip.delete(self.m_page)
-        del self.m_page
+        # stop javascript execution in start script
+        # and delete all the pages C++ objects,
+        # then delete the Python references
+        for page in self.m_pages:
+            sip.delete(page)
+        del self.m_page, self.m_pages
 
         QApplication.instance().exit(code)
 
