776da4987f0814a173c37b6e035b117bcbe9a6ec
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-08-20 23:11:14 -0700

    Add JSLint directives for the bootstrap code.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,6 @@
+/*jslint sloppy: true, nomen: true */
+/*global window:true,phantom:true,fs:true */
+
 // This allows creating a new web page using the construct "new WebPage",
 // which feels more natural than "phantom.createWebPage()".
 window.WebPage = function() {
