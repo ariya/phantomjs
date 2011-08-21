2dbdc3659825bd66e11d7d5c87f7973b1ac76114
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-08-20 23:21:39 -0700

    Reduce JSLint warnings on the bootstrap code.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,12 +3,12 @@
 
 // This allows creating a new web page using the construct "new WebPage",
 // which feels more natural than "phantom.createWebPage()".
-window.WebPage = function() {
+window.WebPage = function () {
     var page = phantom.createWebPage(),
         handlers = {};
 
     function defineSetter(handlerName, signalName) {
-        page.__defineSetter__(handlerName, function(f) {
+        page.__defineSetter__(handlerName, function (f) {
             if (handlers && typeof handlers[signalName] === 'function') {
                 try {
                     this[signalName].disconnect(handlers[signalName]);
@@ -17,7 +17,7 @@ window.WebPage = function() {
             handlers[signalName] = f;
             this[signalName].connect(handlers[signalName]);
         });
-    };
+    }
 
     // deep copy
     page.settings = JSON.parse(JSON.stringify(phantom.defaultPageSettings));
@@ -56,23 +56,23 @@ window.WebPage = function() {
             this.openUrl(arguments[0], {
                 operation: arguments[1],
                 data: arguments[2]
-                }, this.settings);
+            }, this.settings);
             return;
         } else if (arguments.length === 4) {
             this.onLoadFinished = arguments[3];
             this.openUrl(arguments[0], {
                 operation: arguments[1],
                 data: arguments[2]
-                }, this.settings);
+            }, this.settings);
             return;
         }
         throw "Wrong use of WebPage#open";
     };
 
-    page.includeJs = function(scriptUrl, onScriptLoaded) {
+    page.includeJs = function (scriptUrl, onScriptLoaded) {
         // Register temporary signal handler for 'alert()'
-        this.javaScriptAlertSent.connect(function(msgFromAlert) {
-            if ( msgFromAlert === scriptUrl ) {
+        this.javaScriptAlertSent.connect(function (msgFromAlert) {
+            if (msgFromAlert === scriptUrl) {
                 // Resource loaded, time to fire the callback
                 onScriptLoaded(scriptUrl);
                 // And disconnect the signal handler
@@ -86,7 +86,7 @@ window.WebPage = function() {
         this._appendScriptElement(scriptUrl);
     };
 
-    page.destroy = function() {
+    page.destroy = function () {
         phantom._destroy(page);
     };
 
@@ -103,12 +103,12 @@ window.WebPage = function() {
  * @param mode Open Mode. A string made of 'r', 'w', 'a/+' characters.
  * @return "file" object
  */
-window.fs.open = function(path, mode) {
+window.fs.open = function (path, mode) {
     var file = window.fs._open(path, mode);
     if (file) {
         return file;
     }
-    throw "Unable to open file '"+ path +"'";
+    throw "Unable to open file '" + path + "'";
 };
 
 /** Open, read and return content of a file.
@@ -117,7 +117,7 @@ window.fs.open = function(path, mode) {
  * @param path Path of the file to read from
  * @return file content 
  */
-window.fs.read = function(path) {
+window.fs.read = function (path) {
     var f = fs.open(path, 'r'),
         content = f.read();
 
@@ -132,7 +132,7 @@ window.fs.read = function(path) {
  * @param content Content to write to the file
  * @param mode Open Mode. A string made of 'w' or 'a / +' characters.
  */
-window.fs.write = function(path, content, mode) {
+window.fs.write = function (path, content, mode) {
     var f = fs.open(path, mode);
 
     f.write(content);
@@ -145,10 +145,10 @@ window.fs.write = function(path, content, mode) {
  * @param path Path fo the file to read the size of
  * @return File size in bytes
  */
-window.fs.size = function(path) {
+window.fs.size = function (path) {
     var size = fs._size(path);
     if (size !== -1) {
         return size;
     }
-    throw "Unable to read file '"+ path +"' size";
+    throw "Unable to read file '" + path + "' size";
 };
