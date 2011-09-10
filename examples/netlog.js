1fd9c20835426d89eea5d4f342558aa61bcc7a8a
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-09-10 09:15:34 -0700

    netlog: Example to dump live network traffic.
    http://code.google.com/p/phantomjs/issues/detail?id=227

---
+++ examples/netlog.js
@@ -0,0 +1,23 @@
+var page = new WebPage(),
+    address = phantom.args[0];
+
+if (phantom.args.length === 0) {
+    console.log('Usage: netlog.js <some URL>');
+    phantom.exit();
+} else {
+
+    page.onResourceRequested = function (req) {
+        console.log('requested: ' + JSON.stringify(req, undefined, 4));
+    };
+
+    page.onResourceReceived = function (res) {
+        console.log('received: ' + JSON.stringify(res, undefined, 4));
+    };
+
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address');
+        }
+        phantom.exit();
+    });
+}
