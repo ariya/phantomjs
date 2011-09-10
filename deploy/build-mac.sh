53b70172a199713f50300319e7b7e7c28b1a4939
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-09-09 22:01:09 -0700

    Mac build script: workaround for qt_menu.nib problem.
    http://code.google.com/p/phantomjs/issues/detail?id=142
    
    See also the Qt bug:
    https://bugreports.qt.nokia.com/browse/QTBUG-5952

--- deploy/build-mac.sh
+++ deploy/build-mac.sh
@@ -27,6 +27,7 @@ mv qt-everywhere-opensource-src-$QT_VERSION Qt-$QT_VERSION
 
 cd $QT_FOLDER
 patch configure ../allow-static-qtwebkit.patch
+patch -p1 < ../qapplication_skip_qtmenu.patch
 echo "Building Qt $QT_VERSION. Please wait..."
 echo
 ./configure -opensource -confirm-license -release -static -no-exceptions -no-stl -no-xmlpatterns -no-phonon -no-qt3support -no-opengl -no-declarative -qt-libpng -qt-libjpeg -no-libmng -no-libtiff -D QT_NO_STYLE_CDE -D QT_NO_STYLE_CLEANLOOKS -D QT_NO_STYLE_MOTIF -D QT_NO_STYLE_PLASTIQUE -cocoa -prefix $PWD -arch x86 -nomake demos -nomake examples -nomake tools
