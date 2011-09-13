26e49cc4c160a6c67534e837282e2ee1519cd56d
IceArmy <roejames12@hotmail.com>
2011-09-13 14:59:59 -0700

    Fix bug on FileSystem() where super() in __new__() was calling the CSConverter class, instead of FileSystem. Whoops!!

--- python/pyphantomjs/filesystem.py
+++ python/pyphantomjs/filesystem.py
@@ -114,7 +114,7 @@ class FileSystem(QObject):
     _instance = None
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(CSConverter, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(FileSystem, cls).__new__(cls, *args, **kwargs)
         return cls._instance
 
     def __init__(self, parent):
