9d06785287696331323282efda4d2fb39f526610
Nick Rowe <nixterrimus@gmail.com>
2011-10-06 13:43:12 -0700

    Change comment in run-jasmine example to be consistent with the way the code is written

--- examples/run-jasmine.js
+++ examples/run-jasmine.js
@@ -30,7 +30,7 @@ function waitFor(testFx, onReady, timeOutMillis) {
                     clearInterval(interval); //< Stop this interval
                 }
             }
-        }, 100); //< repeat check every 250ms
+        }, 100); //< repeat check every 100ms
 };
 
 
