18d1533429889faf56b0e222c50992677cbc6e4f
Milian Wolff <mail@milianw.de>
2011-11-02 15:20:53 +0100

    exit rasterize.js example when the address could not be loaded

--- examples/rasterize.js
+++ examples/rasterize.js
@@ -17,6 +17,7 @@ if (phantom.args.length < 2 || phantom.args.length > 3) {
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
+            phantom.exit();
         } else {
             window.setTimeout(function () {
                 page.render(output);
