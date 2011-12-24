7592806fb86017f523ecafe45eef7fc5b6f22a68
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-12-23 21:08:42 -0800

    Current master is for PhantomJS 1.5.

--- src/consts.h
+++ src/consts.h
@@ -32,11 +32,11 @@
 #ifndef CONSTS_H
 #define CONSTS_H
 
-// Current Version: 1.4.0 (unstable)
+// Current Version: 1.5.0
 #define PHANTOMJS_VERSION_MAJOR     1
-#define PHANTOMJS_VERSION_MINOR     4
+#define PHANTOMJS_VERSION_MINOR     5
 #define PHANTOMJS_VERSION_PATCH     0
-#define PHANTOMJS_VERSION_STRING    "1.4.0"
+#define PHANTOMJS_VERSION_STRING    "1.5.0 (development)"
 
 #define COFFEE_SCRIPT_EXTENSION     ".coffee"
 
