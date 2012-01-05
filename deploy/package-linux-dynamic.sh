380352156c43614d58a355a60a099ff354f0b3f6
Jon Leighton <j@jonathanleighton.com>
2012-01-05 23:19:33 +0000

    Deploy script refactoring and improvements.

---
+++ deploy/package-linux-dynamic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd `dirname $0`/..
+
+mkdir phantomjs
+cp -r examples ChangeLog LICENSE.BSD README.md bin phantomjs
+
+QT_LIB=deploy/Qt-4.8.0/lib
+mkdir phantomjs/lib
+cp -r $QT_LIB/libQtCore.* $QT_LIB/libQtGui.* $QT_LIB/libQtNetwork.* $QT_LIB/libQtWebKit.* phantomjs/lib
+
+chrpath -r \$ORIGIN/../lib phantomjs/bin/phantomjs
+tar -czf phantomjs.tar.gz phantomjs
+rm -r phantomjs/
