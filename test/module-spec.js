2a109186cc5011192a02183cb5bbc2b732aa813d
execjosh <buildjosh@gmail.com>
2012-01-12 20:28:25 +0900

    Add system to module-spec.js

--- test/module-spec.js
+++ test/module-spec.js
@@ -12,6 +12,10 @@ describe("module loading using require", function() {
         expect(typeof require('fs')).toEqual('object');
     });
 
+    it("should work for 'system' module", function() {
+        expect(typeof require('system')).toEqual('object');
+    });
+
     it("should throw an error for an unknown module", function() {
         var module = 'foobar';
         expect(function(){
