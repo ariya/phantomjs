2787e2085016696bf36de968b607158567119a2f
Milian Wolff <mail@milianw.de>
2012-01-15 19:24:46 +0100

    introduce separate postserver.js example, simplify simpleserver.js again

--- examples/simpleserver.js
+++ examples/simpleserver.js
@@ -27,11 +27,6 @@ if (phantom.args.length !== 1) {
         response.write('<pre>');
         response.write(JSON.stringify(request, null, 4));
         response.write('</pre>');
-        response.write('<p>Test POST:</p>');
-        response.write('<form action="#" method="post" accept-charset="UTF-8">');
-        response.write('<input name="foo" type="submit" value="submit" />');
-        response.write('<input name="my name" type="text" />');
-        response.write('</form>');
         response.write('</body>');
         response.write('</html>');
     });
