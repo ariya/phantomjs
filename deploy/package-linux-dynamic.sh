a398b3fec0625ecbd3772f0b6094929dda38d482
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-03-08 21:58:30 -0800

    Revamp the build scripts in deploy/.
    Technically only deploy/package-linux-dynamic.sh is relevant and it's
    been adjusted appropriately. All other files are obsoleted with the new
    build process.
    
    http://code.google.com/p/phantomjs/issues/detail?id=226

--- deploy/package-linux-dynamic.sh
+++ deploy/package-linux-dynamic.sh
@@ -5,7 +5,7 @@ cd `dirname $0`/..
 mkdir phantomjs
 cp -r examples ChangeLog LICENSE.BSD README.md bin phantomjs
 
-QT_LIB=deploy/Qt-4.8.0/lib
+QT_LIB=src/qt/lib
 mkdir phantomjs/lib
 cp -r $QT_LIB/libQtCore.* $QT_LIB/libQtGui.* $QT_LIB/libQtNetwork.* $QT_LIB/libQtWebKit.* phantomjs/lib
 
