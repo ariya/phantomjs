8673597bc4e3a4d9f66e92be36d0e81fc200b181
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-03-15 08:12:12 -0700

    Fix mistake in 'require' logic.
    (as spotted by Nicolas Perriault).
    
    http://code.google.com/p/phantomjs/issues/detail?id=270

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -37,7 +37,7 @@ function require(name) {
 
     var code, func, exports;
 
-    if (name === 'webpage' || name === 'fs' || name === 'webserver' | name === 'system') {
+    if (name === 'webpage' || name === 'fs' || name === 'webserver' || name === 'system') {
         code = phantom.loadModuleSource(name);
         func = new Function("exports", "window", code);
         exports = {};
