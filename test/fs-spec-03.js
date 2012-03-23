03e47a1712ce7dd6f5db20000f708cfb7705da85
Ivan De Marino <ivan.de.marino@gmail.com>
2012-03-23 11:45:13 +0000

    Fixing defect in the "Filesystem Spec 03".
    Too strong assumption regarding the Current Working Directory.

--- test/fs-spec-03.js
+++ test/fs-spec-03.js
@@ -1,14 +1,13 @@
 describe("Files and Directories API", function() {
 	var TEST_DIR = "testdir",
 		TEST_FILE = "testfile",
-		START_CWD = null;
-	
-	it("should create a new temporary directory and change the Current Working Directory to it", function() {
 		START_CWD = fs.workingDirectory;
+
+	it("should create a new temporary directory and change the Current Working Directory to it", function() {
 		expect(fs.makeDirectory(TEST_DIR)).toBeTruthy();
 		expect(fs.changeWorkingDirectory(TEST_DIR)).toBeTruthy();
 	});
-	
+
 	it("should create a file in the Current Working Directory and check it's absolute path", function() {
 		fs.write(TEST_FILE, TEST_FILE, "w");
 		var suffix = fs.separator + TEST_DIR + fs.separator +  TEST_FILE,
@@ -17,18 +16,16 @@ describe("Files and Directories API", function() {
 		expect(lastIndex).toNotEqual(-1);
 		expect(lastIndex + suffix.length === abs.length);
 	});
-	
+
 	it("should return to previous Current Working Directory and remove temporary directory", function() {
 		expect(fs.changeWorkingDirectory(START_CWD)).toBeTruthy();
 		fs.removeTree(TEST_DIR);
 	});
 
-	it("should copy Current Working Directory tree in a temporary directory, compare with the original and remove", function() {
-		expect(fs.changeWorkingDirectory("../")).toBeTruthy();
-		fs.copyTree(START_CWD, TEST_DIR);
-		expect(fs.list(START_CWD).length).toEqual(fs.list(TEST_DIR).length);
+	it("should copy Content of the '/test/' Directory in a temporary directory, compare with the original and then remove", function() {
+		fs.copyTree(phantom.libraryPath, TEST_DIR);
+		expect(fs.list(phantom.libraryPath).length).toEqual(fs.list(TEST_DIR).length);
 		fs.removeTree(TEST_DIR);
-		expect(fs.changeWorkingDirectory(START_CWD)).toBeTruthy();
 	});
 
     // TODO: test the actual functionality once we can create symlink.
