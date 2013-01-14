02a616e30ef2409431926449b0c3cb2961ecee18
James M. Greene <james.m.greene@gmail.com>
2013-01-14 12:55:20 -0600

    Added myself to the copyright list

--- src/config.cpp
+++ src/config.cpp
@@ -4,6 +4,7 @@
   Copyright (C) 2012 Ariya Hidayat <ariya.hidayat@gmail.com>
   Copyright (C) 2011 Ariya Hidayat <ariya.hidayat@gmail.com>
   Copyright (C) 2011 execjosh, http://execjosh.blogspot.com
+  Copyright (C) 2013 James M. Greene <james.m.greene@gmail.com>
 
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions are met:
