1044e06078ff4305b5bba41bfbe10206ce19750b
juliankrispel <juliankrispel@yahoo.de>
2013-07-03 12:07:58 +0100

    encode URI before opening

--- examples/rasterize.js
+++ examples/rasterize.js
@@ -7,7 +7,7 @@ if (system.args.length < 3 || system.args.length > 5) {
     console.log('  paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"');
     phantom.exit(1);
 } else {
-    address = system.args[1];
+    address = encodeURIComponent(system.args[1]);
     output = system.args[2];
     page.viewportSize = { width: 600, height: 600 };
     if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
