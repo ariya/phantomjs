d2879715737cf2cc83c277cd28e177fca09126bc
FreshXOpenSource <support@freshx.de>
2013-07-26 03:09:50 +0200

    Fix for PDF prints on Mac OSX. Produces selectable text

--- src/webpage.cpp
+++ src/webpage.cpp
@@ -1067,6 +1067,14 @@ bool WebPage::renderPdf(const QString &fileName)
     QPrinter printer;
     printer.setOutputFormat(QPrinter::PdfFormat);
     printer.setOutputFileName(fileName);
+
+    // Fix for PDF prints on Mac OSX. Produces selectable text
+    #ifdef Q_WS_MACX
+        if(fileName.endsWith(".pdf")) {
+            printer.setOutputFormat(QPrinter::NativeFormat);
+        }
+    #endif
+
     printer.setResolution(PHANTOMJS_PDF_DPI);
     QVariantMap paperSize = m_paperSize;
 
