aac74b57e49ffb31340292f86581c979333d2580
Dmitry Parshin <dparshin@smartling.com>
2013-09-10 16:05:44 -0400

    Fixed body string initialization code (0 byte for empty strings) Also fixed same code in filesystem module
    https://github.com/ariya/phantomjs/issues/10158

--- src/networkreplyproxy.cpp
+++ src/networkreplyproxy.cpp
@@ -59,7 +59,7 @@ QString NetworkReplyProxy::body()
 {
     //converting data to QString in a special way(without charset encoding), 
     //similar to File::read method in 'filesystem' module.
-    QString ret(m_data.size());
+    QString ret(m_data.size(), ' ');
     for(int i = 0; i < m_data.size(); ++i) {
         ret[i] = m_data.at(i);
     }
