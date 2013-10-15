9982cb9efa5004499912d0849ed8954dc5cbbc53
jun.zhu <jun.zhu@zaptechnology.com>
2013-10-15 14:04:44 +1000

    Fix margin, don't know where the 2.54 is from!!

--- src/webpage.cpp
+++ src/webpage.cpp
@@ -1028,7 +1028,7 @@ QImage WebPage::renderImage()
     return buffer;
 }
 
-#define PHANTOMJS_PDF_DPI 72            // Different defaults. OSX: 72, X11: 75(?), Windows: 96
+#define PHANTOMJS_PDF_DPI 96            // Different defaults. OSX: 72, X11: 75(?), Windows: 96
 
 qreal stringToPointSize(const QString &string)
 {
@@ -1039,8 +1039,9 @@ qreal stringToPointSize(const QString &string)
         { "mm", 72 / 25.4 },
         { "cm", 72 / 2.54 },
         { "in", 72 },
-        { "px", 72.0 / PHANTOMJS_PDF_DPI / 2.54 },
-        { "", 72.0 / PHANTOMJS_PDF_DPI / 2.54 }
+        { "px", 72.0 / PHANTOMJS_PDF_DPI },
+        { "pt", 1 },
+        { "", 72.0 / PHANTOMJS_PDF_DPI }
     };
     for (uint i = 0; i < sizeof(units) / sizeof(units[0]); ++i) {
         if (string.endsWith(units[i].unit)) {
