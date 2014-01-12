e9bb1ca7c6168e7e073d9253cbe93e9655f9812d
Ivan De Marino <detronizator@gmail.com>
2014-01-12 21:08:21 +0000

    Importing GhostDriver 1.1.1.
    Yes, 1.1.0 has just been imported.
    But the key feature in 1.1.1 is Session Isolation in WebDriver: something that has been requested many times,
    particularly when using GhostDriver with Selenium Grid.

--- src/ghostdriver/main.js
+++ src/ghostdriver/main.js
@@ -35,7 +35,7 @@ ghostdriver = {
     hub     : require("./hub_register.js"),
     logger  : require("./logger.js"),
     config  : null,                         //< this will be set below
-    version : "1.1.0"
+    version : "1.1.1"
 };
 
 // create logger
