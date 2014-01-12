056aa50c19b31de967ec7c30e59c6cb16680e48d
Ivan De Marino <detronizator@gmail.com>
2014-01-12 21:08:21 +0000

    Importing GhostDriver 1.1.1.
    Yes, 1.1.0 has just been imported.
    But the key feature in 1.1.1 is Session Isolation in WebDriver: something that has been requested many times,
    particularly when using GhostDriver with Selenium Grid.

--- src/ghostdriver/session.js
+++ src/ghostdriver/session.js
@@ -101,6 +101,7 @@ ghostdriver.Session = function(desiredCapabilities) {
     },
     _windows = {},  //< NOTE: windows are "webpage" in Phantom-dialect
     _currentWindowHandle = null,
+    _cookieJar = require('cookiejar').create(),
     _id = require("./third_party/uuid.js").v1(),
     _inputs = ghostdriver.Inputs(),
     _capsPageSettingsPref = "phantomjs.page.settings.",
@@ -284,8 +285,12 @@ ghostdriver.Session = function(desiredCapabilities) {
     _addNewPage = function(newPage) {
         _log.debug("_addNewPage");
 
-        _decorateNewWindow(newPage);                //< decorate the new page
-        _windows[newPage.windowHandle] = newPage;   //< store the page/window
+        // decorate the new Window/Page
+        newPage = _decorateNewWindow(newPage);
+        // set session-specific CookieJar
+        newPage.cookieJar = _cookieJar;
+        // store the Window/Page by WindowHandle
+        _windows[newPage.windowHandle] = newPage;
     },
 
     // Delete any closing page from the "_windows" container of this session
@@ -487,9 +492,15 @@ ghostdriver.Session = function(desiredCapabilities) {
 
         // Ensure a Current Window is available, if it's found to be `null`
         if (_currentWindowHandle === null) {
-            // First call to get the current window: need to create one
-            page = _decorateNewWindow(require("webpage").create());
+            // Create the first Window/Page
+            page = require("webpage").create();
+            // Decorate it with listeners and helpers
+            page = _decorateNewWindow(page);
+            // set session-specific CookieJar
+            page.cookieJar = _cookieJar;
+            // Make the new Window, the Current Window
             _currentWindowHandle = page.windowHandle;
+            // Store by WindowHandle
             _windows[_currentWindowHandle] = page;
         }
     },
@@ -616,6 +627,9 @@ ghostdriver.Session = function(desiredCapabilities) {
         for (k in _windows) {
             _closeWindow(k);
         }
+
+        // Release CookieJar resources
+        _cookieJar.close();
     },
 
     _getLog = function (type) {
