77e53071dc47169cb24ec746643a40219712c4fd
Ariya Hidayat <ariya.hidayat@gmail.com>
2014-01-18 14:15:56 -0800

    Getting ready for 1.9.3.
    https://github.com/ariya/phantomjs/issues/11904

--- test/phantom-spec.js
+++ test/phantom-spec.js
@@ -55,8 +55,8 @@ describe("phantom global object", function() {
         expect(phantom.version.minor).toEqual(9);
     });
 
-    it("should return 2 as the patch version", function() {
-        expect(phantom.version.patch).toEqual(2);
+    it("should return 3 as the patch version", function() {
+        expect(phantom.version.patch).toEqual(3);
     });
 
     it("should have 'injectJs' function", function() {
