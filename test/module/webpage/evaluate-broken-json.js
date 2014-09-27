7bcb9a8725eb6e0c76a35f806d5b0a1b628e67a5
Ariya Hidayat <ariya.hidayat@gmail.com>
2014-09-27 13:21:29 -0700

    Add a test to demonstrate the problem with JSON-ification.
    https://github.com/ariya/phantomjs/issues/12615

---
+++ test/module/webpage/evaluate-broken-json.js
@@ -0,0 +1,14 @@
+var assert = require('../../assert');
+var webpage = require('webpage');
+
+var page = webpage.create();
+
+// Hijack JSON.parse to something completely useless.
+page.content = '<html><script>JSON.parse = function() {}</script></html>';
+
+var result = page.evaluate(function(obj) {
+    return obj.value * obj.value;
+}, { value: 4 });
+
+// FAIL
+// assert.equal(result, 16);
