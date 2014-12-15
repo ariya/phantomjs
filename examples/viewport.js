b1ef55585adc6ffa077977e52732f405f443059e
Chase Sillevis <chase@sillevis.net>
2014-12-15 10:39:28 +0100

    Add a viewport example

---
+++ examples/viewport.js
@@ -0,0 +1,23 @@
+var page = require('webpage').create(),
+	address, output, size;
+
+if (phantom.args.length < 4 || phantom.args.length > 5) {
+    console.log('Usage: rasterize.js URL filename sizeX sizeY');
+    phantom.exit();
+} else {
+    address = phantom.args[0];
+    output = phantom.args[1];
+    sizeX = phantom.args[2];
+    sizeY = phantom.args[3];
+    page.viewportSize = { width: sizeX, height: sizeY };
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('Unable to load the address!');
+        } else {
+            window.setTimeout(function () {
+                page.render(output);
+                phantom.exit();
+            }, 200);
+        }
+    });
+}
