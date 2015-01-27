ea61616abf8006f0c6a7eabc994e188bd530af72
jefleponot <jef_le_ponot@voila.fr>
2015-01-27 18:38:35 +0100

    add simple feature: set Text to renders

--- src/webpage.h
+++ src/webpage.h
@@ -242,7 +242,7 @@ public slots:
     void close();
 
     QVariant evaluateJavaScript(const QString &code);
-    bool render(const QString &fileName, const QVariantMap &map = QVariantMap());
+    bool render(const QString &fileName, const QVariantMap &map = QVariantMap(), const QVariantMap &info = QVariantMap());
     /**
      * Render the page as base-64 encoded string.
      * Default image format is "png".
