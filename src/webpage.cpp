0984f1618ecb549f83cb14a1d1066cdcb79614c8
Todor Petrov <t.petrov@viscomp.bg>
2015-02-09 14:09:56 +0200

    Background color should be white, when rendering page in JPEG format

--- src/webpage.cpp
+++ src/webpage.cpp
@@ -1037,7 +1037,7 @@ QImage WebPage::renderImage()
 #endif
 
     QImage buffer(frameRect.size(), format);
-    buffer.fill(Qt::transparent);
+    buffer.fill(QColor(255, 255, 255, 255));
 
     QPainter painter;
 
@@ -1051,7 +1051,7 @@ QImage WebPage::renderImage()
         for (int y = 0; y < vtiles; ++y) {
 
             QImage tileBuffer(tileSize, tileSize, format);
-            tileBuffer.fill(Qt::transparent);
+            tileBuffer.fill(QColor(255, 255, 255, 255));
 
             // Render the web page onto the small tile first
             painter.begin(&tileBuffer);
