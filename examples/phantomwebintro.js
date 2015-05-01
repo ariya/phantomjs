afd679297b5a833a2c7558bb2f1b9ffae7d7b3d5
jmiller76 <jmiller76@gmail.com>
2015-05-01 11:50:32 -0500

    phantomwebintro.js demo fix for #13196
    Tested on Windows 7 with 2.0 release.

--- examples/phantomwebintro.js
+++ examples/phantomwebintro.js
@@ -6,7 +6,7 @@ page.onConsoleMessage = function(msg) {
     console.log(msg);
 };
 
-page.open("http://www.phantomjs.org", function(status) {
+page.open("http://phantomjs.org", function(status) {
     if ( status === "success" ) {
         page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
             page.evaluate(function() {
