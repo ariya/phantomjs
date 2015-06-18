9870a1f9d16c5d8c0f5d5737fccbc38c68e93942
Vitalii Grygoruk <vitaliy.grigoruk@gmail.com>
2015-06-18 09:52:17 +0200

    mkrpm.sh syntax errors fixed

--- rpm/mkrpm.sh
+++ rpm/mkrpm.sh
@@ -15,11 +15,11 @@ mkdir -p ${buildroot} ${builddir}
 echo "=> Copying sources..."
 ( cd .. && tar cf - ./[A-Z]* ./bin ./examples | tar xf - -C ${builddir} )
 echo "=> Creating source tarball under ${sourcedir}..."
-( cd ${builddir}/.. && zip -r ${sourcedir}/${name}-${version}-source.zip ${name}-${version}
+( cd ${builddir}/.. && zip -r ${sourcedir}/${name}-${version}-source.zip ${name}-${version} )
 echo "=> Building RPM..."
 rpmbuild --define "_topdir ${topdir}" --buildroot ${buildroot} --clean -bb ${name}.spec
 RPMS=(`ls ${topdir}/RPMS/*/*.rpm`)
-cp ${topdir}/RPMS/*/*.rpm 
+cp ${topdir}/RPMS/*/*.rpm ${TMPDIR:-/tmp}/
 rm -fr ${topdir}
 for rpm in "${RPMS[@]}"; do
     echo ${TMPDIR:-/tmp}/${rpm##*/}
