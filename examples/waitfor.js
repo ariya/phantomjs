3457b4a3e0205eb7270e859da69ce02c01def6d5
Paul Verest <enide.github@gmail.com>
2015-10-14 12:17:00 +0800

    waitfor.js example - use JSDoc

--- examples/waitfor.js
+++ examples/waitfor.js
@@ -2,10 +2,10 @@
  * Wait until the test condition is true or a timeout occurs. Useful for waiting
  * on a server response or for a ui change (fadeIn, etc.) to occur.
  *
- * @param testFx javascript condition that evaluates to a boolean,
+ * @param {Function|string} testFx javascript condition that evaluates to a boolean,
  * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
  * as a callback function.
- * @param onReady what to do when testFx condition is fulfilled,
+ * @param {Function|string} onReady what to do when testFx condition is fulfilled,
  * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
  * as a callback function.
  * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
