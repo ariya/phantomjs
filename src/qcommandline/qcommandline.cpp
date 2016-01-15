65bc139bfc064606e2f19c03f3bf4e5470099817
Alhadis <gardnerjohng@gmail.com>
2016-01-16 02:52:26 +1100

    Fix typo in command-line help message
    Closes #13894

--- src/qcommandline/qcommandline.cpp
+++ src/qcommandline/qcommandline.cpp
@@ -437,7 +437,7 @@ QCommandLine::help(bool logo)
     h += QFileInfo(d->args[0]).baseName();
   else
     h += QCoreApplication::applicationName();
-  h.append(QLatin1String(" [switchs] [options]"));
+  h.append(QLatin1String(" [switches] [options]"));
   /* Arguments, short */
   foreach (QCommandLineConfigEntry entry, d->config) {
     if (entry.type == QCommandLine::Option) {
