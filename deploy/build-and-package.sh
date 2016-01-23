1f312c141f5abfd67dd14d64db9848c33980e9fb
Ariya Hidayat <ariya.hidayat@gmail.com>
2016-01-23 14:02:21 -0800

    Fix OS X packaging
    Refs #12970

--- deploy/build-and-package.sh
+++ deploy/build-and-package.sh
@@ -2,19 +2,5 @@
 
 cd `dirname $0`/..
 
-echo "Building Qt and PhantomJS with debugging symbols. If you have previously" \
-     "built without debugging symbols, you should run:"
-echo
-echo "    $ git clean -xdff"
-echo
+./build.py --confirm --release --git-clean-qtbase --git-clean-qtwebkit "$@" || exit 1
 
-./build.py --confirm --release-debug "$@" || exit 1
-
-# Package the release tarball
-rm deploy/*.tar.bz2 2>/dev/null
-./deploy/package.sh || exit 1
-
-echo "PhantomJS built and packaged:"
-echo
-cd deploy
-ls -1 *.tar.bz2
