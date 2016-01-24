d9cda3dcd26b0e463533c5cc96e39c0f39fc32c1
Ariya Hidayat <ariya.hidayat@gmail.com>
2016-01-24 10:19:22 -0800

    Set version to "2.1.1"
    Refs #12970

--- test/basics/version.js
+++ test/basics/version.js
@@ -3,5 +3,5 @@
 test(function () {
     assert_equals(phantom.version.major, 2);
     assert_equals(phantom.version.minor, 1);
-    assert_equals(phantom.version.patch, 0);
+    assert_equals(phantom.version.patch, 1);
 }, "PhantomJS version number is accurate");
