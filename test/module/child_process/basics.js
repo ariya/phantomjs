cad46a54954aa995c34bb8c621e02cd77800309b
Tobias Dammers <t.dammers@thauris.nl>
2016-02-03 09:43:01 +0100

    Changed child_process tests to use platform-agnostic python scripts
    Previous version depended on system-provided `cat` and `echo` commands,
    but these aren't portable across OS platforms. Now using
    platform-agnostic, 2/3-agnostic python scripts, which should work on
    anything that has a somewhat recent python interpreter installed.

--- test/module/child_process/basics.js
+++ test/module/child_process/basics.js
@@ -1,10 +1,34 @@
 // Test basic child process functionality
 //
 
+var ECHO_SCRIPT = 'echo.py';
+var CAT_SCRIPT = 'cat.py';
+
+setup(function(){
+    var f;
+    var fs = require('fs');
+
+    // Platform-agnostic drop-in replacements for 'echo -n' and 'cat'.
+    // Using sys.stdout.write rather than print here to make the
+    // python code 2/3 agnostic.
+
+    f = fs.open(ECHO_SCRIPT, 'w');
+    f.writeLine('import sys');
+    f.writeLine('');
+    f.writeLine('sys.stdout.write(" ".join(sys.argv[1:]))');
+    f.close();
+
+    f = fs.open(CAT_SCRIPT, 'w');
+    f.writeLine('import sys');
+    f.writeLine('');
+    f.writeLine('sys.stdout.write(sys.stdin.read())');
+    f.close();
+});
+
 async_test(function(){
     var text = "hello";
     var process = require('child_process');
-    var p = process.spawn("echo", ["-n", text]);
+    var p = process.spawn("python", [ECHO_SCRIPT, text]);
     var out = '';
     p.stdout.on('data', function(data) { out += data; });
     p.on('exit', this.step_func_done(
@@ -17,7 +41,7 @@ async_test(function(){
 async_test(function(){
     var text = "hello";
     var process = require('child_process');
-    var p = process.spawn("cat", []);
+    var p = process.spawn("python", [CAT_SCRIPT]);
     var out = '';
     p.stdout.on('data', function(data) { out += data; });
     p.on('exit', this.step_func_done(
