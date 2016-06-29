c056aece3f1e82dc67d8c13121b7ae4ec6d8b55a
Maksim Baldin <baldin@embria.com>
2016-06-29 16:56:55 +0300

    system _phantomjs variable name changed to Ariel

--- src/webpage.cpp
+++ src/webpage.cpp
@@ -74,9 +74,9 @@
 
 // Ensure we have at least head and body.
 #define BLANK_HTML                      "<html><head></head><body></body></html>"
-#define CALLBACKS_OBJECT_NAME           "_phantom"
-#define INPAGE_CALL_NAME                "window.callPhantom"
-#define CALLBACKS_OBJECT_INJECTION      INPAGE_CALL_NAME" = function() { return window."CALLBACKS_OBJECT_NAME".call.call(_phantom, Array.prototype.slice.call(arguments, 0)); };"
+#define CALLBACKS_OBJECT_NAME           "_ariel"
+#define INPAGE_CALL_NAME                "window.callAriel"
+#define CALLBACKS_OBJECT_INJECTION      INPAGE_CALL_NAME" = function() { return window."CALLBACKS_OBJECT_NAME".call.call(_ariel, Array.prototype.slice.call(arguments, 0)); };"
 #define CALLBACKS_OBJECT_PRESENT        "typeof(window."CALLBACKS_OBJECT_NAME") !== \"undefined\";"
 
 #define STDOUT_FILENAME "/dev/stdout"
