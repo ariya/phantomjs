f3e81b84e8543cbbd933f38e85453b892848e495
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-10-01 18:37:01 +0300

    Bump version in tests

--- test/basics/version.js
+++ test/basics/version.js
@@ -2,6 +2,6 @@
 // updated with every release.
 test(function () {
     assert_equals(phantom.version.major, 2);
-    assert_equals(phantom.version.minor, 2);
+    assert_equals(phantom.version.minor, 5);
     assert_equals(phantom.version.patch, 0);
 }, "PhantomJS version number is accurate");
