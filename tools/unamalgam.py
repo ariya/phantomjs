a81a38ffabe2cea715ff1c6ab9425f006c3eb166
pixiupl <devs4p@protonmail.com>
2018-01-15 18:36:55 +0100

    Refactoring code #5

--- tools/unamalgam.py
+++ tools/unamalgam.py
@@ -6,8 +6,8 @@ import os
 cur_src = None
 in_mod = False
 ofile = None
-
 strip_re = re.compile(r'/\* Amalgamated: (.*) \*/')
+
 def clean(l):
     return strip_re.sub(r'\1', l)
 
@@ -19,25 +19,17 @@ with open(fname) as f:
             l = next(f)
             g = re.match(r'#line [01] "(.*)"', l)
             cur_src = g.group(1)
-
-            # if there is currently opened file, close it
             if ofile:
-                ofile.close()
-
+            ofile.close()
             cur_src = re.sub(r'\.\./', '', cur_src)
-
-            # create directory for the next file if needed
             cur_src_dir = os.path.dirname(cur_src)
             if cur_src_dir != '' and not os.path.exists(cur_src_dir):
-                os.makedirs(cur_src_dir)
-
-            # open next file for writing
+            os.makedirs(cur_src_dir)
             ofile = open(cur_src, "w")
             print >>sys.stderr, '=> %s' % cur_src
             manifest.append(cur_src)
             next(f)
         elif ofile:
             ofile.write(clean(l))
-
 m = open('%s.manifest' % os.path.basename(fname), 'w')
 print >>m, '\n'.join(manifest)
