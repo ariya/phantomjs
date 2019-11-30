1c53215f8e0f23344a4626d38117d3dc53982f02
Ariya Hidayat <ariya.hidayat@gmail.com>
2019-11-29 22:07:37 -0800

    Mark as version 3.0.0-development

--- src/consts.h
+++ src/consts.h
@@ -32,10 +32,10 @@
 #ifndef CONSTS_H
 #define CONSTS_H
 
-#define PHANTOMJS_VERSION_MAJOR     2
-#define PHANTOMJS_VERSION_MINOR     2
+#define PHANTOMJS_VERSION_MAJOR     3
+#define PHANTOMJS_VERSION_MINOR     0
 #define PHANTOMJS_VERSION_PATCH     0
-#define PHANTOMJS_VERSION_STRING    "2.2.0-development"
+#define PHANTOMJS_VERSION_STRING    "3.0.0-development"
 
 #define HTTP_HEADER_CONTENT_LENGTH      "content-length"
 #define HTTP_HEADER_CONTENT_TYPE        "content-type"
--- test/basics/version.js
+++ test/basics/version.js
@@ -1,7 +1,7 @@
 // This is separate from basics/phantom-object.js because it has to be
 // updated with every release.
 test(function () {
-    assert_equals(phantom.version.major, 2);
-    assert_equals(phantom.version.minor, 2);
+    assert_equals(phantom.version.major, 3);
+    assert_equals(phantom.version.minor, 0);
     assert_equals(phantom.version.patch, 0);
 }, "PhantomJS version number is accurate");
